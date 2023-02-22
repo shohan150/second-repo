e7aea8d6d7019abab9d4149b3fed2f5ff8680a7f
shohan <shohan.ece17@gmail.com>
2023-02-22 18:39:15 +0600

    lll

--- java01/1.js
+++ java01/1.js
@@ -1,1 +1,8 @@
-console.log(5);
+const sections = document.querySelectorAll('section');
+for (const section of sections) {
+    section.style.border = '2px solid steelblue';
+    section.style.marginBottom = '10px'
+
+}
+
+const placesContainer = document.getElementById('fruits-container') 
